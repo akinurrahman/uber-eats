0614765cbf3a2d9dbfde8f6878d22be73db729f0
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-10-26 16:45:29 +0530

    headline cards done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import HeadLineCards from './components/HeadLineCards'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <HeadLineCards/>
     </div>
   )
 }
